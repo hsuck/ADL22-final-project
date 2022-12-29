2d17fe56f8e2c5c3bd32e521136d2a3c7a4c4265
ricky <cc2297.fn@gmail.com>
2022-12-29 06:47:14 +0000

    [Training] Sampling over Subgroup

--- python/random_50.py
+++ python/random_50.py
@@ -19,16 +19,24 @@ data_dir = Path("../data")
 
 def initialize():
     train_df = pd.read_csv( str(data_dir/'train.csv') ).fillna('')
-    course_df = pd.read_csv( str(data_dir/"courses.csv") )
+    course_df = pd.read_csv( str(data_dir/"courses.csv") ).fillna('')
     user_plog = {
         uid: cid_list.split(' ') for uid, cid_list in zip(train_df['user_id'], train_df['course_id'])
     }
     course_feat = {
         feat['course_id']: feat.drop('course_id').to_dict() for feat in course_df.iloc
     }
-    return user_plog, course_feat
 
-user_plog, course_feat = initialize()
+    subgroup_df = pd.read_csv( str(data_dir/'subgroups.csv') )
+    sg2id = {
+        name: id for name, id in zip( subgroup_df['subgroup_name'], subgroup_df['subgroup_id'])
+    }
+    sg2id[''] = -1
+    
+    return user_plog, course_feat, sg2id
+
+user_plog, course_feat, sg2id = initialize()
+
 
 #######################
 ### Utils 
@@ -121,28 +129,39 @@ def get_pubtime_distribution():
         s = np.sum(year_prob[year-2015])
         year_prob[year-2015] = year_prob[year-2015] / s
     
-    month_prob = np.mean( year_prob[1:-1,:], axis=0)
+    month_prob_arr = np.mean( year_prob[1:-1,:], axis=0)
+    month_prob_arr[ month_prob_arr == 0 ] = np.min(month_prob_arr) / 5
+    def pfunc(year, month, month_prob_arr):
+        """
+            month in [1, 12]
+        """
+        year_weight = np.max( [ 2*np.exp(year-2015), 1e-9])
+        return month_prob_arr[ month - 1 ] * year_weight
 
-    return Counter(res), month_prob
+    prob_func = partial(
+        pfunc,
+        month_prob_arr = month_prob_arr,
+    )
+
+    return Counter(res), prob_func
 
 def draw_pubtime_distribution():
-    pubtime_cnt, month_prob = get_pubtime_distribution()
+    pubtime_cnt, prob_func = get_pubtime_distribution()
 
-    def time_to_int( t, year_offset = 2014, mon_offset = 12 ):
-        year = int(t[:4])
-        month = int(t[-2:])
-        return (year - year_offset) * 12 + (month - mon_offset)
-    
     pubtime = []
     cnt = []
+    prob = []
     for year in range(2015, 2022):
         for month in range(1, 13):
             t = f"{year}-{month}"
             pubtime.append(t)
             cnt.append(pubtime_cnt[t])
+            prob.append( prob_func(year, month) ) 
         
         print(pubtime[-12:])
         print(cnt[-12:])
+    prob = np.array(prob)
+
     total = np.sum(cnt)
     cnt = np.array(cnt) / total
 
@@ -151,11 +170,8 @@ def draw_pubtime_distribution():
     plt.bar(
         x = pubtime,
         height = cnt,
+        label = 'train'
     )
-
-    prob = np.zeros( (len(pubtime, )) )
-    for i in range( 0, prob.shape[0], 12 ):
-        prob[i:i+12] = month_prob
 
     prob = prob / np.sum(prob)
 
@@ -164,6 +180,7 @@ def draw_pubtime_distribution():
         x = pubtime,
         height = prob,
         alpha=0.5,
+        label = 'estimate'
     )
 
     ymin, ymax = plt.ylim()
@@ -175,31 +192,93 @@ def draw_pubtime_distribution():
         linestyles=['dotted'] * len(date_sep)
     )
 
+    plt.legend()
     plt.title("P( course is be bought | Publis Time)")
 
     plt.savefig("pubtime_distribution.png")
 
+
+
+#######################
+### Joint Price & Pub-Time Distribution
+#######################
+def get_joint_price_pubtime_distribution():
+    pcnt = get_purchase_count()
+
+    def def_value(): return 0
+    res = defaultdict(def_value)
+
+    for cid, cnt in pcnt.items():
+        price = course_feat[cid]['course_price']
+        time = course_feat[cid]['course_published_at_local'][:7]
+        year, month = int(time[:4]), int(time[-2:])
+        res[(price,year,month)] += cnt
+
+    return Counter(res)
+
+def draw_joint_price_pubtime_distribution():
+    pp_cnt = get_joint_price_pubtime_distribution()
+    total_cnt = np.sum( list(pp_cnt.values()) )
+
+    print(pp_cnt)
+
+#######################
+### Subgroup Distribution
+#######################
+def get_subgroup_distribution():
+    pcnt = get_purchase_count()
+
+    def def_value(): return 0
+    res = defaultdict(def_value)
+
+
+    for cid, cnt in pcnt.items():
+        sgs = [ sg2id[sg] for sg in course_feat[cid]['sub_groups'].split(',') ]
+        for sg in sgs:
+            res[sg] += cnt
+        
+    total = np.sum( list(res.values()) )
+    category_prob = {
+        key: val / total for key, val in res.items()
+    }
+    def pfunc( subgroups: str, category_prob: Dict[int, float] ):
+        # subgroups = "生活,語言,..."
+        sgs = [ sg2id[sg] for sg in subgroups.split(',')]
+        probs = [ category_prob[i] for i in sgs]
+        return np.mean(probs)
+
+    
+    prob_func = partial(
+        pfunc,
+        category_prob=category_prob
+    )
+    return Counter(res), prob_func
+
+def draw_joint_price_pubtime_distribution():
+    pp_cnt = get_joint_price_pubtime_distribution()
+    total_cnt = np.sum( list(pp_cnt.values()) )
+
+    print(pp_cnt)
 
 #######################
 ### Inference & Eval
 #######################
 
 def get_course_prob():
-    _, prob_func = get_price_distribution()
-    _, month_prob = get_pubtime_distribution()
+    _, price_pfunc = get_price_distribution()
+    _, pub_pfunc = get_pubtime_distribution()
+    _, sg_pfunc = get_subgroup_distribution()
     course_prob = {}
 
-    def year_weight( y ):
-        pivot = 2015
-        dy = y - pivot
-        return np.exp(2*dy)
 
     for cid, feat in course_feat.items():
         try:
+            price = feat['course_price']
             time = feat['course_published_at_local'][:7]
             y = int(time[:4])
-            m = int(time[-2:])-1
-            course_prob[cid] = prob_func( feat['course_price'] ) * month_prob[m] * year_weight(y)
+            m = int(time[-2:])
+            sg_str = feat['sub_groups']
+            course_prob[cid] = price_pfunc( price ) * pub_pfunc(y, m) * sg_pfunc(sg_str)
         except Exception as e:
             print(e)
             course_prob[cid] = 0
@@ -284,36 +363,42 @@ if __name__ == "__main__":
         topic seen:     0.0780 - 0.0850 / 0.07525
         topic unseen:   0.0710 - 0.0750 / 0.08913
     """
+    # draw_joint_price_pubtime_distribution()
 
     course2prob = get_course_prob()
     courses = list(course2prob.keys())
     prob = normalize(list(course2prob.values()))
 
-
+    from utils.submission_format import course_to_topic_pred
+    
     output_dir = Path("../output/month_price_sample")
     eval_files = ['val_seen.csv', 'val_unseen.csv' ]
     for name in eval_files:
         file_path = data_dir / name
         output_path = generate_course_pred_csv( file_path, courses, prob, output_dir )
-        
-        print(f"{file_path.stem}:\n  mAP@50: {evaluate_map( str(file_path), str(output_path) )}")
+
+        topic_actual = data_dir / f"{file_path.stem}_group.csv"
+        topic_pred = output_dir / f"{file_path.stem}_group_pred.csv"
+        course_to_topic_pred(
+                str(output_path),
+                "../data/courses.csv",
+                "../data/subgroups.csv",
+        ).to_csv( str(topic_pred), index=False )
+
+        print(f"{file_path.stem}:")
+        print(f"  course mAP@50: {evaluate_map( str(file_path), str(output_path) )}")
+        print(f"  topic  mAP@50: {evaluate_map( str(topic_actual), str(topic_pred) )}")
 
     test_files = ['test_seen.csv', 'test_unseen.csv' ]
     for name in test_files:
         file_path = data_dir / name
         
-        print(f"Inference {file_path.stem}")
+        print(f"Inference {file_path.stem} Course / Topic Prediction ...")
         output_path = generate_course_pred_csv( file_path, courses, prob, output_dir )
-
-    from utils.submission_format import course_to_topic_pred
-
-    course_to_topic_pred(
-        "../output/month_price_sample/test_seen_pred.csv",
-        "../data/courses.csv",
-        "../data/subgroups.csv",
-    ).to_csv("../output/month_price_sample/test_seen_group_pred.csv", index=False)
-    course_to_topic_pred(
-        "../output/month_price_sample/test_unseen_pred.csv",
-        "../data/courses.csv",
-        "../data/subgroups.csv",
-    ).to_csv("../output/month_price_sample/test_unseen_group_pred.csv", index=False)
+        topic_pred = output_dir / f"{file_path.stem}_group_pred.csv"
+        course_to_topic_pred(
+                str(output_path),
+                "../data/courses.csv",
+                "../data/subgroups.csv",
+        ).to_csv( str(topic_pred), index=False )
+        print("Finish")
